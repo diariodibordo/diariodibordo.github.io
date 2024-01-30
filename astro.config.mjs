2a0b3174e6ea753d3f7e30c7174e9f8b7617cfa0
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2024-01-30 23:00:56 +0100

    italian language as default

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,34 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://diariodibordo.github.io',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: '📝 Logs',
-					autogenerate: { directory: 'logs' },
-				},
-				{
-					label: 'Manuale',
-					autogenerate: { directory: 'rules' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://diariodibordo.github.io',
+  integrations: [starlight({
+    title: 'Diario di Bordo',
+    locales: {
+      root: {
+        label: 'Italiano',
+        lang: 'it-IT'
+      }
+    },
+    social: {
+      github: 'https://github.com/manuelpagliuca'
+    },
+    sidebar: [{
+      label: 'Manuale',
+      autogenerate: {
+        directory: 'rules'
+      },
+    }, {
+      label: '📝 Logs',
+      autogenerate: {
+        directory: 'logs'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })
+]
 });
