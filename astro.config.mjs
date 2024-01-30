1b2f7fd0a3f01ac7b9ee5d98788f73ecf7f6051c
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2024-01-30 22:02:56 +0100

    init

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://diariodibordo.github.io',
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Logs',
+					autogenerate: { directory: 'logs' },
+				},
+				{
+					label: 'Manuale',
+					autogenerate: { directory: 'rules' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
